1162b489c0f3c61528e3bd84d747c4620deafe1f
bowler <1835597031@qq.com>
2022-07-01 20:43:56 +0800

    作业

---
+++ src/侦听器.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <span>全选:</span>
+    <input type="text" v-model="name" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+    };
+  },
+  watch: {
+    // 可以监听某个值的变化
+    name(newVal, oldVal) {
+      // newVal 改变之后的值
+      // oldVal 改变之前的值
+      console.log('newVal', newVal);
+      console.log('oldVal', oldVal);
+    },
+    // name(val) {
+    //   // 当这一块只用了一个星参数 是改变之后的值
+    //   console.log('newVal', val);
+    // },
+  },
+  // 目标: 侦听到name值的改变
+  /*
+  语法:
+    watch: {
+      变量名 (newVal, oldVal){
+        // 变量名对应值改变这里自动触发
+        // newVal 是改变之后的值
+        // oldVal 是改变之前的值  
+      }
+    }
+  */
+};
+</script>
---
+++ src/过滤器.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ msg | toDown }}</h1>
+    <h2 v-bind:title="msg | toUp">{{ msg | toUp | toReverse }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  filters: {
+    // 过滤器名称(val) {
+    //   return 处理后的数据;
+    // },
+    toUp(val) {
+      console.log(val);
+      return val.toUpperCase();
+    },
+  },
+  data() {
+    return {
+      msg: 'hello world',
+    };
+  },
+};
+</script>
+
+<style></style>
